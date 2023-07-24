aea4d6d37365d12c7d67462d1ffad42433471cab
Reinhardkipkoech <rtowett14@gmail.com>
2023-07-24 13:07:51 -0700

    betty documentation for task 0 and task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - function that sets pointe to 98
+ * @n: integer input by user
  * Return: Always 0
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * swap_int - function that swaps the valus of two integers
+ * @a: first integer input
+ * @b: second integer input
  */
 void swap_int(int *a, int *b)
 {
